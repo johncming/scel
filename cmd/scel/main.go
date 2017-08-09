2708a32a95f11db64e7f22e9b9c4c71a4e3cd360
johncming <johncming@yahoo.com>
2017-08-09 14:28:07 +0800

    cmd/scel added.

---
+++ cmd/scel/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	"encoding/json"
+
+	"fmt"
+
+	"flag"
+
+	"bitbucket.org/johncming/scel"
+)
+
+var scelPath string
+
+func init() {
+	flag.StringVar(&scelPath, "p", "", "scel path")
+	flag.Parse()
+}
+
+func readScel() ([]byte, error) {
+	return ioutil.ReadFile(scelPath)
+}
+
+func main() {
+	data, err := readScel()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	scel := scel.NewScel(data)
+	err = scel.Run()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	output, err := json.Marshal(scel.PyTable)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%s\n", string(output))
+}
